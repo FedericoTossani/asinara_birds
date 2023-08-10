370764bb41ace236dec71fa65c4592d4c1c4c533
FedericoTossani <fede.tossani@gmail.com>
2023-08-10 15:41:05 +0200

    Update R_code_relaz23.r

--- R_code_relaz23.r
+++ R_code_relaz23.r
@@ -17,10 +17,15 @@ data <- data%>%
 
 # MIGRATORI
 
-rondine <- data%>%
-          #filter(NOME.COMUNE == "Rondine")%>%
-          ggplot(aes(x = date, fill = "NOME.COMUNE"))+
-          geom_bar(position="stack")+
+# Color Palette
+
+col <- colorRampPalette(c("#5a8300", "#6474ff", "#4acf50", "#8d3ec4", "#1c8700", "#cb4cd0", "#d0cb3b", "#5a39b7", "#898c00", "#cf81ff", "#006c15", "#ff61ca", "#01b582", "#f11683", "#87d987", "#bd0027", "#19dbe5", "#f5473a", "#068eff", "#ffb24e", "#404e8f", "#ae7400", "#31c5ff", "#b13500", "#009067", "#a80040", "#006637", "#ffa3ed", "#506100", "#863079", "#d2c96e", "#813968", "#acb67e", "#8a364c", "#018880", "#ff7e72", "#476031", "#b89bce", #7d441d", "#df99b4", "#ff988e", "#f3a8a3"))
+
+
+all_plot <- data%>%
+          arrange(date, desc(captures))%>%
+          ggplot(aes(x = date, y = captures, fill = NOME.COMUNE))+
+          geom_col()+
           theme_light()+
           theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
