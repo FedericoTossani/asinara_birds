d210e00daf9c48153f5f95b0ed2d4505d9421729
FedericoTossani <fede.tossani@gmail.com>
2023-08-15 18:09:24 +0200

    Update R_code_relaz23.r

--- R_code_relaz23.r
+++ R_code_relaz23.r
@@ -21,27 +21,78 @@ data <- data%>%
 
 col <- colorRampPalette(c("#5a8300", "#6474ff", "#4acf50", "#8d3ec4", "#1c8700", "#cb4cd0", "#d0cb3b", "#5a39b7", "#898c00", "#cf81ff", "#006c15", "#ff61ca", "#01b582", "#f11683", "#87d987", "#bd0027", "#19dbe5", "#f5473a", "#068eff", "#ffb24e", "#404e8f", "#ae7400", "#31c5ff", "#b13500", "#009067", "#a80040", "#006637", "#ffa3ed", "#506100", "#863079", "#d2c96e", "#813968", "#acb67e", "#8a364c", "#018880", "#ff7e72", "#476031", "#b89bce", #7d441d", "#df99b4", "#ff988e", "#f3a8a3"))
 
+          
+          all_plot <- data%>%
+                    arrange(date, desc(captures))%>%
+                    ggplot(aes(x = date, y = captures, fill = NOME.COMUNE))+
+                    geom_col()+
+                    theme_light()+
+                    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
-all_plot <- data%>%
-          arrange(date, desc(captures))%>%
-          ggplot(aes(x = date, y = captures, fill = NOME.COMUNE))+
+ rondine <- data%>%
+          filter(NOME.COMUNE == "Rondine")%>%
+          ggplot(aes(x = date, y = captures))+
           geom_col()+
           theme_light()+
-          theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+          theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
+          labs(title = "Rondine", subtitle = "Hirundo rustica")
+                          
+luigrosso <- data%>%
+          filter(NOME.COMUNE == "Luì grosso")%>%
+          ggplot(aes(x = date, y = captures))+
+          geom_col()+
+          theme_light()+
+          theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
+          labs(title = "Luì grosso", subtitle = "Phylloscopus trochilus")
 
-luigrosso <-
+codirossocomune <- data%>%
+          filter(NOME.COMUNE == "Codirosso comune")%>%
+          ggplot(aes(x = date, y = captures))+
+          geom_col()+
+          theme_light()+
+          theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
+          labs(title = "Codirosso comune", subtitle = "Phoenicurus phoenicurus")
 
-codirossocomune <-
+beccafico <- data%>%
+          filter(NOME.COMUNE == "Beccafico")%>%
+          ggplot(aes(x = date, y = captures))+
+          geom_col()+
+          theme_light()+
+          theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
+          labs(title = "Beccafico", subtitle = "Sylvia borin")
 
-beccafico <-
+canapinomaggiore <- data%>%
+          filter(NOME.COMUNE == "Canapino maggiore")%>%
+          ggplot(aes(x = date, y = captures))+
+          geom_col()+
+          theme_light()+
+          theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
+          labs(title = "Canapino maggiore", subtitle = "Hippolais icterina")
 
-canapinomaggiore <-
+sterpazzola <- data%>%
+          filter(NOME.COMUNE == "Sterpazzola")%>%
+          ggplot(aes(x = date, y = captures))+
+          geom_col()+
+          theme_light()+
+          theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
+          labs(title = "Sterpazzola", subtitle = "Sylvia communis")
 
-sterpazzola <-
+pigliamosche <- data%>%
+          filter(NOME.COMUNE == "Pigliamosche")%>%
+          ggplot(aes(x = date, y = captures))+
+          geom_col()+
+          theme_light()+
+          theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
+          labs(title = "Pigliamosche", subtitle = "Muscicapa striata")
 
-pigliamosche <-
-
-balianera <-
+balianera <- data%>%
+          filter(NOME.COMUNE == "Balia nera")%>%
+          ggplot(aes(x = date, y = captures))+
+          geom_col()+
+          theme_light()+
+          theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
+          labs(title = "Balia nera", subtitle = "Ficedula hypoleuca")+
+          scale_x_continuous(limits = date, breaks = date)
 
 
 #RESIDENTI
